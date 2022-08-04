e5b5dfe66ffc58ce92588b2aaefd48ba37e8996e
Gema-Munoz <gema.m.moscoso@gmail.com>
2022-08-04 17:58:36 +0200

    iteration 1-2

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,26 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    let directors = moviesArray.map(function(movie){
+        return movie.director
+    })
+
+    return directors
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    let stevenMovies = moviesArray.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"))
+    return stevenMovies.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    //primero tenemos que sumar todos los valores con reduce
+    //el valor retornado por reduce (la suma) la dividimos entre el numero totoal de peliculas (length) para obtener la media
+    // el ejercicio nos pide que retornemos el valor con dos decimales. Hay una funcion que hace eso.
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
